f5f85da42f3812448983754c8782b6b317b31ea2
n8fr8 <nathan@freitas.net>
2015-07-30 08:31:18 -0400

    fix issue with styles and colors

--- awesomeAppCore/src/main/java/org/awesomeapp/messenger/MainActivity.java
+++ awesomeAppCore/src/main/java/org/awesomeapp/messenger/MainActivity.java
@@ -75,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.awesome_activity_main);
 
