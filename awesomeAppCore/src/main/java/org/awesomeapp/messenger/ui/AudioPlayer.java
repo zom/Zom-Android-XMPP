7360df33aed76b85cb6fd1747f850845fca279ef
n8fr8 <nathan@freitas.net>
2015-08-08 02:14:33 -0400

    fix audioplayer to load more smoothly

---
+++ awesomeAppCore/src/main/java/org/awesomeapp/messenger/ui/AudioPlayer.java
@@ -0,0 +1,174 @@
+package org.awesomeapp.messenger.ui;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.media.audiofx.Visualizer;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.awesomeapp.messenger.ui.widgets.VisualizerView;
+import org.awesomeapp.messenger.util.HttpMediaStreamer;
+
+public class AudioPlayer {
+    private static final String TAG = "AudioPlayer";
+
+    private Context mContext;
+    private String mFileName;
+    private String mMimeType;
+
+    private MediaPlayer mediaPlayer;
+    private HttpMediaStreamer streamer;
+
+    private Visualizer mVisualizer;
+    private VisualizerView mVisualizerView;
+
+    private int mDuration = -1;
+    private boolean mPrepared = false;
+    private boolean mPlayOnPrepare = false;
+
+    public AudioPlayer(Context context, String fileName, String mimeType, VisualizerView visualizerView) throws Exception {
+        mContext = context.getApplicationContext();
+        mFileName = fileName;
+        mMimeType = mimeType;
+        mVisualizerView = visualizerView;
+        initPlayer();
+    }
+
+    public int getDuration ()
+    {
+        return mDuration;
+    }
+
+    public void play() {
+
+        if (!mPrepared) {
+
+            mPlayOnPrepare = true;
+
+            try {
+                if (streamer == null)
+                    initPlayer();
+
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+        else {
+
+
+            if (mVisualizer == null)
+                setupVisualizerFxAndUI();
+
+            if (!mVisualizer.getEnabled())
+                mVisualizer.setEnabled(true);
+
+            mediaPlayer.start();
+        }
+    }
+
+    public void pause() {
+        mediaPlayer.pause();
+    }
+
+    public boolean isPlaying () {
+        if (mediaPlayer != null)
+            return mediaPlayer.isPlaying();
+        else
+            return false;
+    }
+
+    public boolean isPaused () {
+        if (mediaPlayer != null)
+            return (!mediaPlayer.isPlaying()) && (mediaPlayer.getCurrentPosition() > 0);
+        else
+            return false;
+    }
+
+    public void stop() {
+        killPlayer();
+    }
+
+    private void killPlayer() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+        if (streamer != null) {
+            streamer.destroy();
+            streamer = null;
+        }
+    }
+
+    public void initPlayer() throws Exception {
+        streamer = new HttpMediaStreamer(mFileName, mMimeType);
+        Uri uri = streamer.getUri();
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setDataSource(mContext, uri);
+
+        mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+
+                Log.d("AudioPlayer", "there was an error loading music: " + i + " " + i1);
+                return true;
+            }
+
+        });
+        mediaPlayer.setOnPreparedListener(new OnPreparedListener() {
+
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+
+                mPrepared = true;
+                mDuration = mediaPlayer.getDuration();
+                if (mPlayOnPrepare)
+                    play();
+
+            }
+        });
+        mediaPlayer.setOnCompletionListener(new OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                //killPlayer();
+                mVisualizer.setEnabled(false);
+
+
+
+            }
+        });
+
+
+        mediaPlayer.prepareAsync();
+
+    }
+
+
+    private void setupVisualizerFxAndUI() {
+
+        // Create the Visualizer object and attach it to our media player.
+        mVisualizer = new Visualizer(mediaPlayer.getAudioSessionId());
+        mVisualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);
+        mVisualizer.setDataCaptureListener(
+                new Visualizer.OnDataCaptureListener() {
+                    public void onWaveFormDataCapture(Visualizer visualizer,
+                                                      byte[] bytes, int samplingRate) {
+                        mVisualizerView.updateVisualizer(bytes);
+                    }
+
+                    public void onFftDataCapture(Visualizer visualizer,
+                                                 byte[] bytes, int samplingRate) {
+                    }
+                }, Visualizer.getMaxCaptureRate() / 2, true, false);
+    }
+}
