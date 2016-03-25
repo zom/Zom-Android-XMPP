307d328c1d887bce80f1fd0c1f06a68b0f7c7cd5
n8fr8 <nathan@freitas.net>
2016-03-25 17:16:02 -0400

    remove image view fullscreen code for now

--- app/src/main/java/org/awesomeapp/messenger/ui/widgets/ImageViewActivity.java
+++ app/src/main/java/org/awesomeapp/messenger/ui/widgets/ImageViewActivity.java
@@ -44,8 +44,15 @@ public class ImageViewActivity extends AppCompatActivity {
         ;
         setTitle("");
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
+       // getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        /**
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);*/
     }
 
 
