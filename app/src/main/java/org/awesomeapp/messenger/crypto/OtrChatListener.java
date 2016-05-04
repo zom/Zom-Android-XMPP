a9b77b6f3847cddd6e3e79ee3c3ac7b18c942fb2
n8fr8 <nathan@freitas.net>
2016-05-04 16:55:09 -0400

    don't auto-refresh when there is an error decryption as this can cause problems the otr engine will handle the errors and requery on its own

--- app/src/main/java/org/awesomeapp/messenger/crypto/OtrChatListener.java
+++ app/src/main/java/org/awesomeapp/messenger/crypto/OtrChatListener.java
@@ -82,10 +82,10 @@ public class OtrChatListener implements MessageListener {
         } catch (OtrException oe) {
 
             OtrDebugLogger.log("error decrypting message from: " + sessionID.getLocalUserId(), oe);
-            mOtrChatManager.refreshSession(sessionID.getLocalUserId(),sessionID.getRemoteUserId());
+          //  mOtrChatManager.refreshSession(sessionID.getLocalUserId(),sessionID.getRemoteUserId());
             // msg.setBody("[" + "You received an unreadable encrypted message" + "]");
             // mMessageListener.onIncomingMessage(session, msg);
-            //   mOtrChatManager.injectMessage(sessionID, "[error please stop/start encryption]");
+            // mOtrChatManager.injectMessage(sessionID, "[error please stop/start encryption]");
 
         }
 
