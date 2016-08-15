3e467a6654c444bc6b15df68fe3f9659bf0c5678
n8fr8 <nathan@freitas.net>
2016-08-15 17:19:02 -0400

    adding new pema stickers

--- app/src/main/java/org/awesomeapp/messenger/ui/ConversationView.java
+++ app/src/main/java/org/awesomeapp/messenger/ui/ConversationView.java
@@ -2969,6 +2969,23 @@ public class ConversationView {
                     sStickerManager.addEmojiToCategory(category, sticker);
                 }
 
+                basePath = "stickers/pema";
+                filelist = aMan.list(basePath);
+
+                category = "Pema";
+
+                for (int i = 0; i < filelist.length; i++) {
+                    Sticker sticker = new Sticker();
+                    sticker.name = filelist[i];
+                    sticker.category = category;
+                    sticker.assetUri = Uri.parse(basePath + '/' +  filelist[i]);
+                    sticker.res = mActivity.getResources();
+                    sticker.emoticon =  filelist[i];
+
+                    sStickerManager.addPattern(sticker.emoticon, sticker);
+                    sStickerManager.addEmojiToCategory(category, sticker);
+                }
+
             }
             catch (Exception fe)
             {
