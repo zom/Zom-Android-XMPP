f7f487dae85dfd5f0ec3ab6da9c24689b27924ae
n8fr8 <nathan@freitas.net>
2017-04-05 23:10:24 -0400

    update libs from smack-omemo project

---
+++ app/libs/update.sh
@@ -0,0 +1,15 @@
+cp ../../../smack/smack-omemo/smack-tcp/build/libs/smack-tcp-4.2.1-SNAPSHOT.jar .
+cp ../../../smack/smack-omemo/smack-repl/build/libs/smack-repl-4.2.1-SNAPSHOT.jar .
+cp ../../../smack/smack-omemo/smack-bosh/build/libs/smack-bosh-4.2.1-SNAPSHOT.jar .
+cp ../../../smack/smack-omemo/smack-sasl-provided/build/libs/smack-sasl-provided-4.2.1-SNAPSHOT.jar .
+cp ../../../smack/smack-omemo/smack-compression-jzlib/build/libs/smack-compression-jzlib-4.2.1-SNAPSHOT.jar .
+cp ../../../smack/smack-omemo/smack-legacy/build/libs/smack-legacy-4.2.1-SNAPSHOT.jar .
+cp ../../../smack/smack-omemo/smack-im/build/libs/smack-im-4.2.1-SNAPSHOT.jar .
+cp ../../../smack/smack-omemo/smack-extensions/build/libs/smack-extensions-4.2.1-SNAPSHOT.jar .
+cp ../../../smack/smack-omemo/smack-resolver-minidns/build/libs/smack-resolver-minidns-4.2.1-SNAPSHOT.jar .
+cp ../../../smack/smack-omemo/smack-core/build/libs/smack-core-4.2.1-SNAPSHOT.jar .
+cp ../../../smack/smack-omemo/smack-experimental/build/libs/smack-experimental-4.2.1-SNAPSHOT.jar .
+cp ../../../smack/smack-omemo/smack-omemo/build/libs/smack-omemo-4.2.1-SNAPSHOT.jar .
+cp ../../../smack/smack-omemo/smack-omemo-signal/build/libs/smack-omemo-signal-4.2.1-SNAPSHOT.jar .
+cp ../../../smack/smack-omemo/smack-android-extensions/build/libs/smack-android-extensions-4.2.1-SNAPSHOT.jar . 
+cp ../../../smack/smack-omemo/smack-android/build/libs/smack-android-4.2.1-SNAPSHOT.jar .
