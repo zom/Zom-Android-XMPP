16eca049de4dd45816a244fc306bb1a175ea0dda
n8fr8 <nathan@freitas.net>
2017-05-08 16:07:16 -0400

    nearly there with #233

--- app/src/main/java/org/awesomeapp/messenger/tasks/MigrateAccountTask.java
+++ app/src/main/java/org/awesomeapp/messenger/tasks/MigrateAccountTask.java
@@ -4,16 +4,20 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.RemoteException;
+import android.text.TextUtils;
 import android.util.Log;
 
 import org.awesomeapp.messenger.ImApp;
 import org.awesomeapp.messenger.crypto.otr.OtrAndroidKeyManagerImpl;
 import org.awesomeapp.messenger.model.ImConnection;
+import org.awesomeapp.messenger.model.ImErrorInfo;
 import org.awesomeapp.messenger.provider.Imps;
 import org.awesomeapp.messenger.service.IChatSession;
 import org.awesomeapp.messenger.service.IChatSessionManager;
 import org.awesomeapp.messenger.service.IContactList;
+import org.awesomeapp.messenger.service.IContactListManager;
 import org.awesomeapp.messenger.service.IImConnection;
 import org.awesomeapp.messenger.service.adapters.ChatSessionAdapter;
 import org.awesomeapp.messenger.ui.legacy.SignInHelper;
@@ -24,6 +28,7 @@ import org.awesomeapp.messenger.ui.onboarding.OnboardingManager;
 import org.json.JSONException;
 
 import java.security.KeyPair;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -38,8 +43,12 @@ public class MigrateAccountTask extends AsyncTask<String, Void, OnboardingAccoun
     long mAccountId;
     long mProviderId;
     ImApp mApp;
+    IImConnection mNewConn;
+    OnboardingAccount mNewAccount;
 
     Handler mHandler = new Handler();
+
+    ArrayList<String> mContacts;
 
     public MigrateAccountTask(Activity context, ImApp app, long providerId, long accountId)
     {
@@ -50,6 +59,7 @@ public class MigrateAccountTask extends AsyncTask<String, Void, OnboardingAccoun
 
         mConn = app.getConnection(providerId, accountId);
 
+        mContacts = new ArrayList<>();
     }
 
     @Override
@@ -68,18 +78,18 @@ public class MigrateAccountTask extends AsyncTask<String, Void, OnboardingAccoun
         String domain = newDomains[0];
 
         //register account on new domain with same password
-        OnboardingAccount account = registerNewAccount(nickname, username, password, domain);
+        mNewAccount = registerNewAccount(nickname, username, password, domain);
 
-        if (account == null) {
+        if (mNewAccount == null) {
 
             username = username + '.' + fingerprint.substring(fingerprint.length()-8,fingerprint.length()).toLowerCase();
-            account = registerNewAccount(nickname, username, password, domain);
+            mNewAccount = registerNewAccount(nickname, username, password, domain);
 
-            if (account == null)
+            if (mNewAccount == null)
                 return null;
         }
 
-        String jabberId = account.username + '@' + account.domain;
+        String jabberId = mNewAccount.username + '@' + mNewAccount.domain;
         keyMan.storeKeyPair(jabberId,keyPair);
 
         //send migration message to existing contacts and/or sessions
@@ -89,57 +99,57 @@ public class MigrateAccountTask extends AsyncTask<String, Void, OnboardingAccoun
             String migrateMessage = "I have moved to a new account at " + jabberId + ". You can add me here: " + inviteLink;
             IChatSessionManager sessionMgr = mConn.getChatSessionManager();
 
-            /**
-            IChatSessionManager sessionMgr = mConn.getChatSessionManager();
-            List<IChatSession> sessions = sessionMgr.getActiveChatSessions();
-
-            for (IChatSession session : sessions)
-            {
-                session.sendMessage(migrateMessage, false);
-            }**/
-
-            List<String> listOfLists = mConn.getContactListManager().getContactLists();
-            for (String listName : listOfLists)
-            {
-                IContactList contactList = mConn.getContactListManager().getContactList(listName);
-                String[] contacts = contactList.getContacts();
-
-                for (String contact : contacts)
-                {
-                    IChatSession session = sessionMgr.getChatSession(contact);
-                    session.sendMessage(migrateMessage, false);
-                }
-            }
-
             //login and set new default account
-
             SignInHelper signInHelper = new SignInHelper(mContext, mHandler);
-            signInHelper.activateAccount(account.providerId, account.accountId);
-            signInHelper.signIn(account.password, account.providerId, account.accountId, true);
+            signInHelper.activateAccount(mNewAccount.providerId, mNewAccount.accountId);
+            signInHelper.signIn(mNewAccount.password, mNewAccount.providerId, mNewAccount.accountId, true);
 
-            IImConnection newConn = mApp.getConnection(account.providerId, account.accountId);
-            newConn.login(account.password, true, true);
+            mNewConn = mApp.getConnection(mNewAccount.providerId, mNewAccount.accountId);
+        //    mNewConn.login(mNewAccount.password, true, true);
 
-            while(newConn.getState() != ImConnection.LOGGED_IN)
+            while(mNewConn.getState() != ImConnection.LOGGED_IN)
             {
                 try { Thread.sleep(500);}
                 catch (Exception e){}
             }
 
-            //add existing contacts on new server
+            List<IContactList> listOfLists = mConn.getContactListManager().getContactLists();
+            for (IContactList contactList : listOfLists)
+            {
+                //IContactList contactList = mConn.getContactListManager().getContactList(listName);
+                String[] contacts = contactList.getContacts();
 
+                for (String contact : contacts)
+                {
+                    mContacts.add(contact);
+
+                    IChatSession session = sessionMgr.getChatSession(contact);
+
+                    if (session == null) {
+                        session = sessionMgr.createChatSession(contact, true);
+                    }
+
+                    session.sendMessage(migrateMessage, false);
+
+                }
+            }
+
+            for (String contact : mContacts)
+            {
+                addToContactList(mNewConn, contact, keyMan.getRemoteFingerprint(contact), null);
+            }
 
             //archive existing conversations and contacts
             List<IChatSession> listSession = mConn.getChatSessionManager().getActiveChatSessions();
             for (IChatSession session : listSession)
             {
-
+                session.leave();
             }
 
             //logout of existing account
             mConn.logout();
 
-            return account;
+            return mNewAccount;
 
 
         }
@@ -157,6 +167,7 @@ public class MigrateAccountTask extends AsyncTask<String, Void, OnboardingAccoun
         super.onPostExecute(account);
 
 
+
     }
 
     private OnboardingAccount registerNewAccount (String nickname, String username, String password, String domain)
@@ -171,5 +182,67 @@ public class MigrateAccountTask extends AsyncTask<String, Void, OnboardingAccoun
         }
 
         return null;
+    }
+
+    private int addToContactList (IImConnection conn, String address, String otrFingperint, String nickname)
+    {
+        int res = -1;
+
+        try {
+
+            IContactList list = getContactList(conn);
+
+            if (list != null) {
+
+                res = list.addContact(address, nickname);
+                if (res != ImErrorInfo.NO_ERROR) {
+
+                    //what to do here?
+                }
+
+                if (!TextUtils.isEmpty(otrFingperint)) {
+                    OtrAndroidKeyManagerImpl.getInstance(mApp).verifyUser(address, otrFingperint);
+                }
+
+                //Contact contact = new Contact(new XmppAddress(address),address);
+                //IContactListManager contactListMgr = conn.getContactListManager();
+                //contactListMgr.approveSubscription(contact);
+            }
+
+        } catch (RemoteException re) {
+            Log.e(ImApp.LOG_TAG, "error adding contact", re);
+        }
+
+        return res;
+    }
+
+    private IContactList getContactList(IImConnection conn) {
+        if (conn == null) {
+            return null;
+        }
+
+        try {
+            IContactListManager contactListMgr = conn.getContactListManager();
+
+            // Use the default list
+            List<IBinder> lists = contactListMgr.getContactLists();
+            for (IBinder binder : lists) {
+                IContactList list = IContactList.Stub.asInterface(binder);
+                if (list.isDefault()) {
+                    return list;
+                }
+            }
+
+            // No default list, use the first one as default list
+            if (!lists.isEmpty()) {
+                return IContactList.Stub.asInterface(lists.get(0));
+            }
+
+            return null;
+
+        } catch (RemoteException e) {
+            // If the service has died, there is no list for now.
+            return null;
+        }
     }
 }
