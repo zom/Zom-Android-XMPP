34921fde22f1c18b09c69e7bc547d96eb7d96104
n8fr8 <nathan@freitas.net>
2017-07-05 10:39:02 -0400

    move back to using self built jars for smack

---
+++ app/libs/update.sh
@@ -0,0 +1,18 @@
+BASELIB="../../../Smack"
+BASEVERSION="4.3.0-alpha1"
+
+cp $BASELIB/smack-core/build/libs/smack-core-$BASEVERSION-SNAPSHOT.jar .
+cp $BASELIB/smack-tcp/build/libs/smack-tcp-$BASEVERSION-SNAPSHOT.jar .
+cp $BASELIB/smack-bosh/build/libs/smack-bosh-$BASEVERSION-SNAPSHOT.jar .
+cp $BASELIB/smack-im/build/libs/smack-im-$BASEVERSION-SNAPSHOT.jar .
+cp $BASELIB/smack-extensions/build/libs/smack-extensions-$BASEVERSION-SNAPSHOT.jar .
+cp $BASELIB/smack-experimental/build/libs/smack-experimental-$BASEVERSION-SNAPSHOT.jar .
+cp $BASELIB/smack-omemo/build/libs/smack-omemo-$BASEVERSION-SNAPSHOT.jar .
+cp $BASELIB/smack-omemo-signal/build/libs/smack-omemo-signal-$BASEVERSION-SNAPSHOT.jar .
+cp $BASELIB/smack-resolver-minidns/build/libs/smack-resolver-minidns-$BASEVERSION-SNAPSHOT.jar .
+cp $BASELIB/smack-sasl-provided/build/libs/smack-sasl-provided-$BASEVERSION-SNAPSHOT.jar .
+cp $BASELIB/smack-compression-jzlib/build/libs/smack-compression-jzlib-$BASEVERSION-SNAPSHOT.jar .
+cp $BASELIB/smack-android/build/libs/smack-android-$BASEVERSION-SNAPSHOT.jar .
+cp $BASELIB/smack-android-extensions/build/libs/smack-android-extensions-$BASEVERSION-SNAPSHOT.jar .
+
+
