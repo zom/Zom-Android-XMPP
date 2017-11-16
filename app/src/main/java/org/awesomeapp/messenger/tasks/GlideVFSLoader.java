a3614ed15867bf6209341e0ea1b79313f5cc1f83
N-Pex <npexdev@gmail.com>
2017-11-16 18:04:59 +0100

    Update Glide library to version 4

--- app/src/main/java/org/awesomeapp/messenger/tasks/GlideVFSLoader.java
+++ app/src/main/java/org/awesomeapp/messenger/tasks/GlideVFSLoader.java
@@ -1,26 +1,22 @@
 package org.awesomeapp.messenger.tasks;
 
-import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Priority;
-import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.Key;
+import com.bumptech.glide.load.Options;
 import com.bumptech.glide.load.data.DataFetcher;
-import com.bumptech.glide.load.model.GenericLoaderFactory;
 import com.bumptech.glide.load.model.ModelLoader;
 import com.bumptech.glide.load.model.ModelLoaderFactory;
-import com.bumptech.glide.load.model.stream.StreamModelLoader;
-import com.bumptech.glide.module.GlideModule;
-import com.bumptech.glide.request.target.ViewTarget;
+import com.bumptech.glide.load.model.MultiModelLoaderFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Date;
+import java.security.MessageDigest;
 
-import im.zom.messenger.R;
-import info.guardianproject.iocipher.File;
 import info.guardianproject.iocipher.FileInputStream;
 
 /**
@@ -28,42 +24,56 @@ import info.guardianproject.iocipher.FileInputStream;
  */
 
 
-public class GlideVFSLoader implements StreamModelLoader<info.guardianproject.iocipher.FileInputStream> {
+public class GlideVFSLoader implements ModelLoader<info.guardianproject.iocipher.FileInputStream, InputStream> {
 
-    private final Context context;
-    public GlideVFSLoader(Context context) {
-        this.context = context.getApplicationContext();
+    public GlideVFSLoader() {
     }
 
-    @Override public DataFetcher<InputStream> getResourceFetcher(final info.guardianproject.iocipher.FileInputStream model, int width, int height) {
-        return new VFSDataFetcher(context, model);
+    @Nullable
+    @Override
+    public LoadData<InputStream> buildLoadData(FileInputStream model, int width, int height, Options options) {
+        return new LoadData<>(new GlideNullCacheKey(), new VFSDataFetcher(model));
+    }
+
+    @Override
+    public boolean handles(FileInputStream model) {
+        return true;
     }
 
     public static class Factory implements ModelLoaderFactory<info.guardianproject.iocipher.FileInputStream, InputStream> {
-        @Override public ModelLoader<FileInputStream, InputStream> build(Context context, GenericLoaderFactory factories) {
-            return new GlideVFSLoader(context);
+        @Override
+        public ModelLoader<FileInputStream, InputStream> build(MultiModelLoaderFactory multiFactory) {
+            return new GlideVFSLoader();
         }
-        @Override public void teardown() {
 
-
+        @Override
+        public void teardown() {
         }
     }
 }
 
+class GlideNullCacheKey implements Key {
+    public GlideNullCacheKey() {
+    }
+
+    @Override
+    public void updateDiskCacheKey(MessageDigest messageDigest) {
+    }
+}
 
 class VFSDataFetcher implements DataFetcher<InputStream> {
-    private final Context context;
     private InputStream vfsFileStream;
 
-    public VFSDataFetcher(Context context, info.guardianproject.iocipher.FileInputStream vfsFileStream) {
+    public VFSDataFetcher(info.guardianproject.iocipher.FileInputStream vfsFileStream) {
         // explode model fields so that they can't be modified (finals in OBBFile are optional)
-        this.context = context;
         this.vfsFileStream = vfsFileStream;
     }
 
-    @Override public InputStream loadData(Priority priority) throws Exception {
-        return vfsFileStream;
+    @Override
+    public void loadData(Priority priority, DataCallback<? super InputStream> callback) {
+        callback.onDataReady(vfsFileStream);
     }
+
     @Override public void cleanup() {
         try {
             if (vfsFileStream != null) {
@@ -73,9 +83,7 @@ class VFSDataFetcher implements DataFetcher<InputStream> {
             Log.w("VFSDataFetcher", "Cannot clean up after stream", e);
         }
     }
-    @Override public String getId() {
-        return context.getPackageName() + "@" + new Date().getTime() + Math.random();
-    }
+
     @Override public void cancel() {
         // do nothing
         try {
@@ -85,5 +93,17 @@ class VFSDataFetcher implements DataFetcher<InputStream> {
         } catch (IOException e) {
             Log.w("VFSDataFetcher", "Cannot clean up after stream", e);
         }
+    }
+
+    @NonNull
+    @Override
+    public Class<InputStream> getDataClass() {
+        return InputStream.class;
+    }
+
+    @NonNull
+    @Override
+    public DataSource getDataSource() {
+        return DataSource.LOCAL;
     }
 }
